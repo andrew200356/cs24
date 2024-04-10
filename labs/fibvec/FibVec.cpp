6bd42f0b6ca1bd7b3dcbfdc642b8037e59f731ac
Andrew Wang <1731095638@qq.com>
2024-04-10 12:05:45 -0700

    lab2 update

--- labs/fibvec/FibVec.cpp
+++ labs/fibvec/FibVec.cpp
@@ -3,5 +3,157 @@
 // This provides exception types:
 #include <stdexcept>
 
-
 // FibVec Function Implementations
+FibVec::FibVec()
+{
+    // Initialize the size and capacity to 0
+    size = 1;
+    m_capacity = 1;
+    num1 = 1;
+    num2 = 1;
+    // Initialize the data array to nullptr
+    m_data = new int[m_capacity];
+}
+
+FibVec::~FibVec()
+{
+    // Delete the data array if it is not nullptr
+    if (m_data != nullptr)
+    {
+        delete[] m_data;
+    }
+}
+
+size_t FibVec::capacity()
+{
+    // Return the capacity of the vector
+    return m_capacity;
+}
+
+size_t FibVec::count()
+{
+    // Return the number of elements in the vector
+    return size;
+}
+
+void FibVec::resize()
+{
+    // Create a new array with the new capacity
+    int *new_data = new int[num1 + num2];
+    num2 = num1 + num2;
+    num1 = num2;
+
+    // Copy the data from the old array to the new array
+    for (size_t i = 0; i < size; i++)
+    {
+        new_data[i] = m_data[i];
+    }
+
+    // Delete the old array
+    delete[] m_data;
+
+    // Update the data pointer and capacity
+    m_data = new_data;
+    m_capacity = num2;
+}
+
+void FibVec::downsize()
+{
+    int *new_data = new int[num1];
+    int a = num1; // num2 now
+    num1 = num2 - num1;
+    num2 = a;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        new_data[i] = m_data[i];
+    }
+
+    delete[] m_data;
+
+    m_data = new_data;
+    m_capacity = num2;
+}
+
+void FibVec::insert(int value, size_t index)
+{
+    // Check if the index is out of bounds
+    if (index > size)
+    {
+        throw std::out_of_range("Index out of bounds");
+    }
+
+    resize();
+
+    // Shift the elements to the right to make room for the new value
+    for (size_t i = size; i > index; i--)
+    {
+        m_data[i] = m_data[i - 1];
+    }
+
+    // Insert the new value at the specified index
+    m_data[index] = value;
+}
+
+size_t FibVec::lookup(size_t index)
+{
+    // Check if the index is out of bounds
+    if (index >= size)
+    {
+        throw std::out_of_range("Index out of bounds");
+    }
+
+    // Return the value at the index given
+    return m_data[index];
+}
+
+int FibVec::pop()
+{
+    // Check if the vector is empty
+    if (size == 0)
+    {
+        throw std::out_of_range("Vector is empty");
+    }
+
+    // Get the last value in the vector
+    int value = m_data[size - 1];
+
+    // Decrement the size of the vector
+    size--;
+
+    downsize();
+
+    // Return the last value in the vector
+    return value;
+}
+
+void FibVec::push(int value)
+{
+    resize();
+
+    // Insert the new value at the end of the vector
+    m_data[size] = value;
+
+    // Increment the size of the vector
+    size++;
+}
+
+void FibVec::remove(size_t index)
+{
+    // Check if the index is out of bounds
+    if (index >= size)
+    {
+        throw std::out_of_range("Index out of bounds");
+    }
+
+    // Shift the elements to the left to remove the value at the specified index
+    for (size_t i = index; i < size - 1; i++)
+    {
+        m_data[i] = m_data[i + 1];
+    }
+
+    // Decrement the size of the vector
+    size--;
+
+    downsize();
+}
--- labs/fibvec/FibVec.h
+++ labs/fibvec/FibVec.h
@@ -4,16 +4,38 @@
 // This provides the size_t type:
 #include <cstddef>
 
-class FibVec {
-  // Member Variables
+/*
+Your vector must obey the following rules:
+    The vector must always have a capacity that is a Fibonacci number.
+    The vector must always have a capacity greater than or equal to f(2) = 1.
+    When inserting into a vector with capacity f(n): if there is no room for the item, the vector must resize its capacity to the next Fibonacci number (f(n+1)).
+    When removing from a vector with capacity f(n): if the number of items drops below f(n-2), the vector must resize its capacity to f(n-1).
+*/
+class FibVec
+{
+    // Member Variables
+    size_t size;
+    size_t m_capacity;
+    int *m_data;
+    int num1;
+    int num2;
 
-  // Helper Functions
+    // Helper Functions
+    void resize();
+    void downsize();
 
 public:
-  // Constructor and Destructor
-
-  // Member Functions
-
+    // Constructor and Destructor
+    FibVec();
+    ~FibVec();
+    // Member Functions
+    size_t capacity();
+    size_t count();
+    void insert(int value, size_t index);
+    size_t lookup(size_t index);
+    int pop();
+    void push(int value);
+    void remove(size_t index);
 };
 
 #endif
